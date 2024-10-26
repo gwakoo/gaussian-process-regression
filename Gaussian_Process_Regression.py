416b77e471ebd5752d6b438a63a8836f72f34018
gwakoo <73497751+gwakoo@users.noreply.github.com>
2024-10-25 23:12:23 -0700

    Update Gaussian_Process_Regression.py

--- Gaussian_Process_Regression.py
+++ Gaussian_Process_Regression.py
@@ -1,10 +1,7 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
- 
-st.write("""
-# My first app
-Hello *world!*
-""")
- 
-df = pd.read_csv("my_data.csv")
-st.line_chart(df)
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, WhiteKernel
+import matplotlib.pyplot as plt
+import yfinance as yf
